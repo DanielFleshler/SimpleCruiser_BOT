8dad9f1424b0c8d844e105bcb38d107a35ad3f74
Daniel Fleshler <danielflesh@my.hit.ac.il>
2024-07-04 16:17:53 +0300

    organized the code

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
-from dotenv import load_dotenv
 import json
+import time
 from math import sqrt, trunc
 from collections import defaultdict
-import time
+
+from dotenv import load_dotenv
 from pyproj import Transformer
 from telegram import (
     InlineKeyboardButton,
@@ -46,27 +47,20 @@ def convert_coordinates(latitude,longitude):
     return easting, northing
 
 # Generate the main menu buttons
-def get_main_menu_buttons(isUserLocation):
-    if isUserLocation:
-        return [
-            [InlineKeyboardButton("🌟 מרכז", callback_data="area:1"), InlineKeyboardButton("🌞 דרום", callback_data="area:2")],
-            [InlineKeyboardButton("🌲 צפון", callback_data="area:3")],
-            [InlineKeyboardButton("🔍 הצג מסלולים לידך", callback_data="showTrails")]
-        ]
+def get_main_menu_buttons(is_user_location):
+    buttons = [
+        [InlineKeyboardButton("🌟 מרכז", callback_data="area:1"), InlineKeyboardButton("🌞 דרום", callback_data="area:2")],
+        [InlineKeyboardButton("🌲 צפון", callback_data="area:3")]
+    ]
+    if is_user_location:
+        buttons.append([InlineKeyboardButton("🔍 הצג מסלולים לידך", callback_data="showTrails")])
     else:
-        return [
-            [InlineKeyboardButton("🌟 מרכז", callback_data="area:1"), InlineKeyboardButton("🌞 דרום", callback_data="area:2")],
-            [InlineKeyboardButton("🌲 צפון", callback_data="area:3")],
-            [InlineKeyboardButton("📍 שתף מיקום", callback_data="userLocation")]
-        ]
+        buttons.append([InlineKeyboardButton("📍 שתף מיקום", callback_data="userLocation")])
+    return buttons
 
 # Generate submenu buttons for a specific area
 def get_submenu_buttons(area, trail_data):
-    area_trail_data = (
-        trail_data['center'] if area == "1"
-        else trail_data['south'] if area == "2"
-        else trail_data['north']
-    )
+    area_trail_data = trail_data['center'] if area == "1" else trail_data['south'] if area == "2" else trail_data['north']
     location_buttons = []
     locations = area_trail_data["locations"]
     for location_name in locations:
@@ -77,17 +71,10 @@ def get_submenu_buttons(area, trail_data):
 
 # Generate difficulty level buttons for a specific location
 def get_difficulty_buttons(area, location, trail_data):
-    area_trail_data = (
-        trail_data['center'] if area == "1"
-        else trail_data['south'] if area == "2"
-        else trail_data['north']
-    )
+    area_trail_data = trail_data['center'] if area == "1" else trail_data['south'] if area == "2" else trail_data['north']
     difficulty_buttons = []
-    difficulty_levels = {
-        'easy': '✊ קל',
-        'medium': '💪 בינוני',
-        'hard': '👊 קשה'
-    }
+    difficulty_levels = {'easy': '✊ קל', 'medium': '💪 בינוני', 'hard': '👊 קשה'}
+
     for level, label in difficulty_levels.items():
         if area_trail_data['locations'][location][level]:
             button_row = [InlineKeyboardButton(label, callback_data=f"difficulty:{area}:{location}:{label}")]
@@ -97,16 +84,14 @@ def get_difficulty_buttons(area, location, trail_data):
 
 # Generate trail links for a specific difficulty level
 def get_trail_links_by_difficulty(area, location, difficulty, trail_data):
-    area_trail_data = (
-        trail_data['center'] if area == "1"
-        else trail_data['south'] if area == "2"
-        else trail_data['north']
-    )
+    area_trail_data = trail_data['center'] if area == "1" else trail_data['south'] if area == "2" else trail_data['north']
+
     difficulty_map = {
         '✊ קל': 'easy',
         '💪 בינוני': 'medium',
         '👊 קשה': 'hard'
     }
+    
     trails = area_trail_data['locations'][location][difficulty_map[difficulty]]
     return [[InlineKeyboardButton(trail['trail_name'], url=trail['location_link'])] for trail in trails]
 
@@ -157,58 +142,22 @@ async def send_welcome_message(update: Update, context: CallbackContext):
         "אם אינך רואה את אפשרות שיתוף המיקום, ייתכן שאין לך הרשאות לשלוח מיקום בצ'אט.\n\n"
         "💪 **בהצלחה!** 🚶‍♂️🌄"
     )
-    await update.message.reply_text(
-        welcome_message,
-        parse_mode='HTML',
-    )    
 
-# Define the callback handler
+    await update.message.reply_text(welcome_message, parse_mode='HTML')    
+
+
+# Callback handlers
 async def handle_button_click(update: Update, context: CallbackContext):
     query = update.callback_query
     data = query.data
     trail_data = context.bot_data["trail_data"]
 
     if data == "userLocationMenu":
-        context.user_data["menu"] = "userLocationMenu"
-        isUserLocation = context.user_data.get("isUserLocation", True)
-        buttons = get_main_menu_buttons(isUserLocation)
-        await query.edit_message_text(
-            "🏞️ <b>בחר אפשרות:</b>", 
-            parse_mode='HTML', 
-            reply_markup=InlineKeyboardMarkup(buttons)
-        )
+        await handle_user_location_menu(update, context)
     elif data == "showTrails":
-        # Show trails near the user using the new function
-        user_location = context.user_data.get("userLocation")
-        if user_location:
-            buttons = show_trails_near_user(update, context)
-            if buttons:
-                await query.edit_message_text(
-                    "**מסלולים בקרבת מקום:**\n\n",
-                    parse_mode='HTML',
-                    reply_markup=InlineKeyboardMarkup(buttons)
-                )
-            else:
-                await query.edit_message_text(
-                    "<b>❌ לא נמצאו מסלולים בקרבתך!</b>\n\n",
-                    parse_mode='HTML', 
-                    reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔙 חזור", callback_data="back")]])
-                )
-        else:
-            await query.edit_message_text(
-                "<b>❌ לא נמצא המיקום שלך! נא שתף את מיקום כדי להמשיך.</b>\n\n",
-                parse_mode='HTML', 
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔙 חזור", callback_data="back")]])
-            )
+        await show_nearby_trails(update, context)
     elif data == "mainMenu":
-        context.user_data["menu"] = "main"
-        isUserLocation = context.user_data.get("isUserLocation", True)
-        buttons = get_main_menu_buttons(isUserLocation)
-        await query.edit_message_text(
-            "🏞️ <b>בחר אפשרות:</b>", 
-            parse_mode='HTML', 
-            reply_markup=InlineKeyboardMarkup(buttons)
-        )
+        await show_main_menu(update, context)
     elif data.startswith("area:"):
         await handle_area_selection(update, context, data, trail_data)
     elif data.startswith("path:"):
@@ -216,15 +165,60 @@ async def handle_button_click(update: Update, context: CallbackContext):
     elif data.startswith("difficulty:"):
         await handle_difficulty_selection(update, context, data, trail_data)
     elif data == "userLocation":
-        # Create a button to share the user's location
-        shareLocation_button = KeyboardButton("📍 שתף את מיקומך", request_location=True)
-        keyboard = ReplyKeyboardMarkup([[shareLocation_button]], resize_keyboard=True, one_time_keyboard=True)
-        await query.message.reply_text(
-            "בחר את מיקומך באמצעות כפתור השיתוף של מיקום",
-            reply_markup=keyboard
-        )
+        await request_user_location(update, context)
     elif data == "back":
         await handle_back_button(update, context, trail_data)
+
+async def show_main_menu(update: Update, context: CallbackContext):
+    context.user_data["menu"] = "main"
+    is_user_location = context.user_data.get("is_user_location", True)
+    buttons = get_main_menu_buttons(is_user_location)
+    await update.callback_query.edit_message_text(
+        "🏞️ <b>בחר אפשרות:</b>", 
+        parse_mode='HTML', 
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+async def handle_user_location_menu(update: Update, context: CallbackContext):
+    context.user_data["menu"] = "userLocationMenu"
+    is_user_location = context.user_data.get("is_user_location", True)
+    buttons = get_main_menu_buttons(is_user_location)
+    await update.callback_query.edit_message_text(
+        "🏞️ <b>בחר אפשרות:</b>", 
+        parse_mode='HTML', 
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+async def request_user_location(update: Update, context: CallbackContext):
+    share_location_button = KeyboardButton("📍 שתף את מיקומך", request_location=True)
+    keyboard = ReplyKeyboardMarkup([[share_location_button]], resize_keyboard=True, one_time_keyboard=True)
+    await update.callback_query.message.reply_text(
+        "בחר את מיקומך באמצעות כפתור השיתוף של מיקום",
+        reply_markup=keyboard
+    )
+
+async def show_nearby_trails(update: Update, context: CallbackContext):
+    user_location = context.user_data.get("userLocation")
+    if user_location:
+        buttons = find_trails_near_user(update, context)
+        if buttons:
+            await update.callback_query.edit_message_text(
+                "**מסלולים בקרבת מקום:**\n\n",
+                parse_mode='HTML',
+                reply_markup=InlineKeyboardMarkup(buttons)
+            )
+        else:
+            await update.callback_query.edit_message_text(
+                "<b>❌ לא נמצאו מסלולים בקרבתך!</b>\n\n",
+                parse_mode='HTML', 
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔙 חזור", callback_data="back")]])
+            )
+    else:
+        await update.callback_query.edit_message_text(
+            "<b>❌ לא נמצא המיקום שלך! נא שתף מיקום כדי להמשיך.</b>\n\n",
+            parse_mode='HTML', 
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔙 חזור", callback_data="back")]])
+        )
 
 
 # Handle area selection
@@ -303,23 +297,22 @@ async def handle_back_button(update: Update, context: CallbackContext, trail_dat
             reply_markup=InlineKeyboardMarkup(buttons)
         )
 
-# Handle location messages
-async def handle_location_message(update: Update, context: CallbackContext, flag=False):
+async def handle_location_message(update: Update, context: CallbackContext, flag=False,SEARCH_RADIUS_KM=10):
     if not flag:
         location = update.message.location
         context.user_data["userLocation"] = location
     else:
         location = context.user_data.get("userLocation")
 
-    easting, northing = convert_coordinates(location.latitude,location.longitude)
+    easting, northing = convert_coordinates(location.latitude, location.longitude)
     print(f"Received location: {location.latitude}, {location.longitude}")
     print(f"Converted location: {easting}, {northing}")
 
-    context.user_data["isUserLocation"] = True
+    context.user_data["is_user_location"] = True
     context.user_data["menu"] = "userLocationMenu"
 
     message = (
-        f"המסלולים שיוצגו יהיו במרחק של עד 10 ק״מ ממיקומך🌲\n\n"
+        f"המעלות שיוצגו יהיו במרחק של עד {SEARCH_RADIUS_KM} ק״מ ממיקומך🌲\n\n"
         f"📍 **מיקום שלך:**\n"
         f"מז: {easting:.2f}\n"
         f"צפ: {northing:.2f}\n"
@@ -343,33 +336,24 @@ async def handle_location_message(update: Update, context: CallbackContext, flag
     else:
         return message, buttons
 
-# Define function to show trails near the user within a radius of 10 km
-def show_trails_near_user(update: Update, context: CallbackContext, radius_km: int = 20):
-
-    # Update User state
-    context.user_data["menu"] = "showTrailsMenu"
-    # Retrieve the user's location from user_data
+# Trail search function
+def find_trails_near_user(update: Update, context: CallbackContext, SEARCH_RADIUS_KM=10):
     location = context.user_data.get("userLocation")
-    
-    # Convert the user's location from latitude and longitude to easting and northing
-    easting, northing = convert_coordinates(location.latitude,location.longitude)
+    if not location:
+        return None
 
+    context.user_data["menu"] = "showTrailsMenu"
+    easting, northing = convert_coordinates(location.latitude, location.longitude)
     easting = trunc(easting)
     northing = trunc(northing)
 
     print(f"Received location: {location.latitude}, {location.longitude}")
     print(f"Converted location: {easting}, {northing}")
 
-    # Define the radius in meters
-    radius_m = radius_km * 1000
-        
-    # List to store trails within the radius
+    radius_m = SEARCH_RADIUS_KM * 1000
     trails_within_radius = []
-
-    #Acess trail data from the bot_data
     trail_data = context.bot_data["trail_data"]
     
-    # Loop through the trail data to find trails within the specified radius
     for area in trail_data:
         for location in trail_data[area]['locations']:
             for difficulty in trail_data[area]['locations'][location]:
@@ -381,19 +365,18 @@ def show_trails_near_user(update: Update, context: CallbackContext, radius_km: i
                         if distance <= radius_m:
                             trails_within_radius.append(trail)                     
     
-    # Check if there are any trails within the radius
     if trails_within_radius:
-        buttons = [[InlineKeyboardButton(trail['trail_name'], url=trail['location_link'])] for trail in trails_within_radius]
+        buttons = [
+            [InlineKeyboardButton(trail['trail_name'], url=trail['location_link'])]
+            for trail in trails_within_radius
+        ]
         buttons.append([InlineKeyboardButton("🔙 חזור", callback_data="back")])
         return buttons
     else:
         return None
 
-
-
-# Main function to start the bot
+# Main function
 def main():
-    # Load bot token
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     trail_data_file = 'trails.json'  
     trail_data = load_trail_data(trail_data_file)
@@ -401,13 +384,13 @@ def main():
     application = Application.builder().token(BOT_TOKEN).build()  
     application.bot_data["trail_data"] = trail_data  
 
-    # Register handlers
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CallbackQueryHandler(handle_button_click))
     application.add_handler(MessageHandler(filters.LOCATION, handle_location_message))
 
-    # Start polling
     application.run_polling()
 
 if __name__ == '__main__':
     main()
+
+
